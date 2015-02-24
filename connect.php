330aa4b9c56075ee10209999ddcfa2a5c0b1a829
ewsmith2000 <ewsmith2000@gmail.com>
2015-02-23 19:27:48 -0500

    Unset PDO error mode to error exception

--- connect.php
+++ connect.php
@@ -4,7 +4,6 @@
 		require_once("config.php");
 		try {
 			$db = new PDO(DB_TYPE . ":" . "host=" . DB_SERVER . ";dbname=" . DATABASE, DB_USER, DB_PASSWORD);
-			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch(PDOException $e) {
 			echo "Error: " . $e->getMessage();
 			die();
